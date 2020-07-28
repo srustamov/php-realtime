4d99fd9733f003d7d05e09a938d76ee5c3bba4b1
srustamov <rustemovv96@gmail>
2020-07-28 15:25:21 +0400

    .

--- up.sh
+++ up.sh
@@ -3,7 +3,7 @@ if test ! -f "$(pwd)/vendor/autoload.php"; then
 fi
 
 
-/usr/bin/php $(pwd)/socket/server.php > /dev/null 2>&1 #&
+/usr/bin/php $(pwd)/socket/server.php > /dev/null 2>&1 &
 /usr/bin/php -S localhost:8000 -t $(pwd)/web > /dev/null  2>&1 #&
 
 
